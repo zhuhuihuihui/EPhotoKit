784ff7db63623365e94dc091df4164a85d2ea424
Scott Zhu <zhuhuihuihui@gmail.com>
2017-03-18 15:47:10 -0700

    remove demo test target, add cocoa framework target

---
+++ EPhotoKit/EPhotoKit.h
@@ -0,0 +1,21 @@
+//
+//  EPhotoKit.h
+//  EPhotoKit
+//
+//  Created by Scott Zhu on 18/03/2017.
+//  Copyright © 2017 Scott Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <EPhotoKit/UIImageViewWithPicker.h>
+#import <EPhotoKit/UIImageView+ImagePicker.h>
+#import <EPhotoKit/UIView+FindUIViewController.h>
+//! Project version number for EPhotoKit.
+FOUNDATION_EXPORT double EPhotoKitVersionNumber;
+
+//! Project version string for EPhotoKit.
+FOUNDATION_EXPORT const unsigned char EPhotoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EPhotoKit/PublicHeader.h>
+
+
